85e476982add0a3e97df4081e6d687a82e914932
pedromathias <pedrohmathias10@gmail.com>
2022-11-16 17:49:21 -0300

    Nomes scroll modal

--- src/components/ModalOptions/ModalOptions.tsx
+++ src/components/ModalOptions/ModalOptions.tsx
@@ -36,55 +36,55 @@ export const ModalOptions = ({ isSelectedModal, setIsSelectedModal }) => {
                   <TouchableOpacity>
                   <View style={styles.whatsappicon}>
                   <FontAwesome name="whatsapp" size={30} color="white" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >WhatsApp</Text>
                   </View>
                   </TouchableOpacity>
                   <TouchableOpacity>
                   <View style={styles.instagramicon}>
                   <AntDesign name="instagram" size={30} color="white" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >Instagram</Text>
                   </View>
                   </TouchableOpacity>
                   <TouchableOpacity>
                   <View style={styles.facebookicon}>
                   <EvilIcons style={styles.facebookiconinside} name="sc-facebook" size={60} color="white" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >Facebook</Text>
                   </View>
                   </TouchableOpacity>
                   <TouchableOpacity>
                   <View style={styles.messengericon}>
                   <FontAwesome5 name="facebook-messenger" size={30} color="#4f66ff" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >Messenger</Text>
                   </View>
                   </TouchableOpacity>
                   <TouchableOpacity>
                   <View style={styles.gmailicon}>
                   <MaterialCommunityIcons name="gmail" size={30} color="black" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >Gmail</Text>
                   </View>
                   </TouchableOpacity>
                   <TouchableOpacity>
                   <View style={styles.twittericon}>
                   <AntDesign name="twitter" size={30} color="white" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >Twitter</Text>
                   </View>
                   </TouchableOpacity>
                   <TouchableOpacity>
                   <View style={styles.telegramicon}>
                   <FontAwesome5 name="telegram-plane" size={30} color="white" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >Telegram</Text>
                   </View>
                   </TouchableOpacity>
                   <TouchableOpacity>
                   <View style={styles.copylinkicon}>
                   <EvilIcons name="link" size={38} color="white" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >Copiar</Text>
                   </View>
                   </TouchableOpacity>
                   <TouchableOpacity>
                   <View style={styles.outrosicon}>
                   <Entypo name="dots-three-horizontal" size={30} color="black" />
-                     <Text style={styles.socialNetworkText} >Enviar</Text>
+                     <Text style={styles.socialNetworkText} >Mais</Text>
                   </View>
                   </TouchableOpacity>
                </ScrollView>
